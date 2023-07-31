78cf61db6a1c29c3da65522370158ba195174338
1705163 <rupalibarnwal1797@gmail.com>
2023-08-01 01:18:20 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,109 @@
+<div id="footer"><!-- #footer Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="row"><!-- row Begin -->
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+               
+               <h4>Pages</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="cart.php">Shopping Cart</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="customer/my_account.php">My Account</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr>
+                
+                <h4>User Section</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="customer_register.php">Register</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg hidden-sm">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="com-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Top Products Categories</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="#">Rakhi</a></li>
+                    <li><a href="#">Paintings</a></li>
+                    <li><a href="#">Wall Hangings</a></li>
+                    <li><a href="#">Candles</a></li>
+                    <li><a href="#">Photo frames</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+              <div class="footer_sub">  
+                <h4>About Us</h4>
+                
+                <p>This is my first e-commerce website design in html and css and also using bx-slider.This is my first e-commerce website design in html and css and also using bx-slider.This is my first e-commerce website design in html and css and also using bx-slider. <span><a href="#">Read more</a></span></p>
+                
+                </div>
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3">
+                
+                <h4>Get The News</h4>
+                
+                <p class="text-muted">
+                    Dont miss our latest update products.
+                </p>
+                
+                <form action="" method="post"><!-- form begin -->
+                    <div class="input-group"><!-- input-group begin -->
+                        
+                        <input type="text" class="form-control" name="email">
+                        
+                        <span class="input-group-btn"><!-- input-group-btn begin -->
+                            
+                            <input type="submit" value="subscribe" class="btn btn-default">
+                            
+                        </span><!-- input-group-btn Finish -->
+                        
+                    </div><!-- input-group Finish -->
+                </form><!-- form Finish -->
+                
+                <hr>
+                
+                <h4>Keep In Touch</h4>
+                
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-google-plus"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>
+                
+            </div>
+        </div><!-- row Finish -->
+    </div><!-- container Finish -->
+</div><!-- #footer Finish -->
+
+
+<div id="copyright"><!-- #copyright Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-left">&copy; 2018 M-Dev Store All Rights Reserve</p>
+            
+        </div><!-- col-md-6 Finish -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-right">Theme by: <a href="#">MrGhie</a></p>
+            
+        </div><!-- col-md-6 Finish -->
+    </div><!-- container Finish -->
+</div><!-- #copyright Finish -->
---
+++ includes/sidebar.php
@@ -0,0 +1,37 @@
+<div class="panel panel-default sidebar-menu"><!-- panel panel-default sidebar-menu Begin -->
+    <div class="panel-heading"><!-- panel-heading Begin -->
+        <h3 class="panel-title">Products Categories</h3>
+    </div><!-- panel-heading Finish -->
+    
+    <div class="panel-body"><!-- panel-body Begin -->
+        <ul class="nav nav-pills nav-stacked category-menu"><!-- nav nav-pills nav-stacked category-menu Begin -->
+            
+            <li><a href="#">Rakhi</a></li>
+            <li><a href="#">Paintings</a></li>
+            <li><a href="#">Cards</a></li>
+            <li><a href="#">Photo Frame</a></li>
+            <li><a href="#">wall-hanging</a></li>
+            
+        </ul><!-- nav nav-pills nav-stacked category-menu Finish -->
+    </div><!-- panel-body Finish -->
+    
+</div><!-- panel panel-default sidebar-menu Finish -->
+
+
+<div class="panel panel-default sidebar-menu"><!-- panel panel-default sidebar-menu Begin -->
+    <div class="panel-heading"><!-- panel-heading Begin -->
+        <h3 class="panel-title">Categories</h3>
+    </div><!-- panel-heading Finish -->
+    
+    <div class="panel-body"><!-- panel-body Begin -->
+        <ul class="nav nav-pills nav-stacked category-menu"><!-- nav nav-pills nav-stacked category-menu Begin -->
+            
+            <li><a href="#">Art</a></li>
+            <li><a href="#">Craft</a></li>
+            <li><a href="#">House Decorate</a></li>
+            <li><a href="#">School</a></li>
+            
+        </ul><!-- nav nav-pills nav-stacked category-menu Finish -->
+    </div><!-- panel-body Finish -->
+    
+</div><!-- panel panel-default sidebar-menu Finish -->
